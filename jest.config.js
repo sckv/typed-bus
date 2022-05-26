53ee962d09b5fe52a31e83d8e154a1fec9652bcb
Konstantin Knyazev <konstantinknyazev@outlook.com>
2022-05-27 00:08:45 +0300

    add concierge

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,3 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coveragePathIgnorePatterns: ['<rootDir>/src/examples'],
-  testMatch: ['<rootDir>/src/**/**.test.ts'],
-  setupFiles: ['<rootDir>/.jest-setup.js'],
+  ...require('code-concierge').jest,
 };
